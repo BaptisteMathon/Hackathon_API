3c262787cd3735edfdc7152fd0c15552693f77ba
Baptiste MATHON <baptistemathon@MacBook-Pro-de-Baptiste.local>
2025-03-26 16:25:57 +0100

    preparation deploiement vercel

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     name: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     profilPic: {type: String, default: 'https://static.vecteezy.com/ti/vecteur-libre/p1/1840612-image-profil-icon-male-icon-human-or-people-sign-and-symbol-vector-gratuit-vectoriel.jpg'},
     isAdmin: {type: Boolean, default: false},
